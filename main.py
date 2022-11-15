f8ff2db6d097deb29bb45bd7eb0ee6846b7a2d24
Alina <114672812+alinaarmut@users.noreply.github.com>
2022-11-15 10:10:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
+
 import re
 
+string = input()
+a = re.findall(r'X-{\\', string)
+print(len(a))
 
-test_main = [
-'Классное слово - обороноспособность, которое должно идти после слов: трава и молоко.',
-'Король провел казнь на глазах у всех.',
-'Ворон сидел и клевал кукурузу.',
-'Молоко пьют с медом, но не пьют с печеньем.',
-'В лес пришел жук и сел на пень.'
-]
 
-def test():
-    for n in range(5):
-        print(f' Результат теста {n + 1}')
-        sogl = 'б-джай-нп-тф-ьь'
-        reg = []
-        glas = 'аеиоюуыёяэ'
-        for i in range(len(glas)):
-            reg.append('[' + sogl + ']*(?:' + glas[i] + '[' + sogl + ']+)*' + glas[i] + '[' + sogl + ']*')
-        match = re.findall(r'\b('+"|".join(reg)+r')\b', test_main[n], re.I)
-        match.sort(key=len)
-        for word in match:
-            print(word)
-        print()
 
-test()
+# X-{\giejgiju\\\38281X-{\  - 2
+# sfhwgfSHFGILSJLX-{\3@&X-{\195%X-{\jndv  - 3
+# &9hdgfvhЯИТМ X-{\$hrjfheiИТМОX-{\ X-{\РВА  - 3
+# ВТИТМИОСИМРЦКЫX-{\X\chs\x\[X-{\   -2
+# ИТМОВТ@22@^X\{DGJF\EJGRYU%ВТИТМИОСИМРЦКЫX-{\X\chs\x\[X-{\ ВТИТМИОСИМРЦКЫX-{\X\chs\x\[X-{\   -4
